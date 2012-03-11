46058774f55aa6f1b44cf7bb0b2c835ade2d977e
sheldon <sheldon@envelop.me>
2012-03-11 12:15:50 -0400

    move to device from browser. and , new timercontrol object

---
+++ source/Timer.js
@@ -0,0 +1,27 @@
+enyo.kind({
+    name: "dili.Timer",
+    components: [
+        {
+            kind: "PalmService",
+            service: " palm//com.palm.service.timeout/",
+            method: "",
+            onSuccess: "",
+            onFailure: ""
+        }
+    ],
+    create: function() {
+    //some initial jobs. set flags.
+    },
+    setAlarm: function() {
+    //setup alarm by palmservice
+    },
+    removeAlarm: function() {
+    //remove Alarm
+    },
+    receiveAlarm: function() {
+    //triggers when alarm, via a callback
+    },
+    showTimer: function() {
+    //to show the Timer
+    } 
+})
