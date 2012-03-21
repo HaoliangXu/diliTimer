25f57ee40833507cfcc60b3237c1c7c057a58731
Xu Haoliang <mayagamer@gmail.com>
2012-03-21 00:12:02 -0400

    complete popup window

--- source/popup/popup.js
+++ source/popup/popup.js
@@ -2,11 +2,14 @@ enyo.kind({
    name: "MyPopup",
    kind: "VFlexBox",
    components:[
-      {content: "This is a popup alert"},
-      {kind:"Button"},
+      {content: "TIMES UP"},
+      {kind:"Button",name: "ButtonOK",caption: "OK", onclick: "buttonClick"},
    ],
    constructor: function() {
       this.inherited(arguments);
       enyo.log("create popup kind")
-   }
+   },
+   buttonClick: function() {
+      window.close();
+   },
 })
